e6f28ad225403e938439d2b087355114b607ddba
maaru <maratarzymatov288@gmail.com>
2025-06-01 01:39:57 +0600

    grub update

--- nixos/modules/boot.nix
+++ nixos/modules/boot.nix
@@ -8,17 +8,25 @@
         customResolution = "1600x900";
       };
 
-      grub = {
-        enable = true;
-        device = "nodev";
-        efiSupport = true;
-        useOSProber = true;
-      };
+      # grub = {
+      #   enable = true;
+      #   device = "nodev";
+      #   efiSupport = true;
+      #   useOSProber = true;
+      # };
 
       efi = {
         efiSysMountPoint = "/boot";
         canTouchEfiVariables = true;
       };
     };
+  };
+  boot.loader.grub = {
+    enable = true;
+    version = 2;
+    efiSupport = true;
+    efiInstallAsRemovable = true;
+    devices = ["/dev/sda"]; # Укажите вашу флешку
+    useOSProber = false;
   };
 }
