e9ca1e4bda25a27e82747d2edf1ba6ee10b52e38
maaru <maratarzymatov288@gmail.com>
2025-06-04 19:31:45 +0600

    change linux kernel

--- nixos/modules/kernel.nix
+++ nixos/modules/kernel.nix
@@ -1,4 +1,6 @@
 {pkgs, ...}: {
   # boot.kernelPackages = pkgs.linuxPackages_zen;
-  boot.kernelPackages = pkgs.linuxPackages_latest;
+  # boot.kernelPackages = pkgs.linuxPackages_latest;
+  boot.kernelPackages = pkgs.linuxPackages_6_4;
+  hardware.firmware = with pkgs; [linux-firmware];
 }
