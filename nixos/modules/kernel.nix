040b73e96ddb732f726ce52c4bff1b2b49a8cae0
maaru <maratarzymatov288@gmail.com>
2025-06-05 04:42:15 +0600

    change kernel

--- nixos/modules/kernel.nix
+++ nixos/modules/kernel.nix
@@ -2,10 +2,5 @@
   boot.kernelPackages = pkgs.linuxPackages_zen;
   # loot.kernelPackages = pkgs.linuxPackages_latest;
 
-  # boot.kernelPackages = pkgs.linuxPackages_lts;
-
   hardware.firmware = with pkgs; [linux-firmware];
-  hardware.enableAllFirmware = true;
-
-  boot.kernelParams = ["iwlwifi.disable_11ax=1"];
 }
